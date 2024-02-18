382c6b71f5d0db43151db474225b18690d3b25bd
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-02-18 14:11:34 +0600

    Global Styles With Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: purple;
+`;
+
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <H1>The Wild Oasis</H1>
+    </>
+  );
 }
 export default App;
